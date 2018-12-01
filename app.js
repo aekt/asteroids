b5a20d4b410c199b377cea1ab6122722e190be10
aekt <yusan-23@hotmail.com>
2018-12-02 03:23:41 +0800

    Implement bullet firing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import { Ship } from "./components/ship.js"
+import { Bullet } from "./components/bullet.js";
 
 let app = new PIXI.Application({
     antialias: true,
@@ -15,17 +16,26 @@ app.renderer.resize(window.innerWidth, window.innerHeight);
 document.body.appendChild(app.view);
 
 let ship = new Ship({ x: 256, y: 256, vx: 0, vy: 0 });
+let bullets = [];
 
 ship.when("a", ship.rotateLeft);
 ship.when("d", ship.rotateRight);
 ship.when("w", ship.accelerate);
 ship.when("s", ship.decelerate);
 ship.once(" ", ship.fire);
+ship.on("fire", () => {
+    let bullet = new Bullet({ x: ship.x, y: ship.y, rotation: ship.rotation });
+    bullets.push(bullet);
+    app.stage.addChild(bullet);
+});
 
 app.stage.addChild(ship);
 
 function gameLoop(delta) {
     ship.update(delta);
+    for (let bullet of bullets) {
+        bullet.update(delta);
+    }
 }
 
 app.ticker.add(gameLoop);
