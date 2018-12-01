f89e6a9013998de430b1706bafc10bf47067362a
aekt <yusan-23@hotmail.com>
2018-12-02 03:29:04 +0800

    Add update action to bullet

--- components/bullet.js
+++ components/bullet.js
@@ -3,13 +3,25 @@ export function Bullet(props) {
         let line = new PIXI.Graphics();
         line.lineStyle(4, 0x66FF33, 1);
         line.moveTo(0, 0);
-        line.lineTo(0, 20);
+        line.lineTo(10, 0);
         return line
     }();
 
     unit.x = props.x;
     unit.y = props.y;
     unit.rotation = props.rotation;
+
+    let velocity = 10;
+    unit.vx = velocity * Math.cos(props.rotation);
+    unit.vy = velocity * Math.sin(props.rotation);
+
+    let decay = delta => Math.pow(1.00, delta);
+    unit.update = delta => {
+        unit.vx *= decay(delta);
+        unit.vy *= decay(delta);
+        unit.x += unit.vx * delta;
+        unit.y += unit.vy * delta;
+    };
 
     return unit;
 }
